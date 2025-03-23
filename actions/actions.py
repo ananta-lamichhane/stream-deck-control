b2db0cc72951e5a62d6998da085868ee60b1158c
ananta-lamichhane <bloomsnruins@gmail.com>
2025-03-23 22:00:05 +0100

    cleanup, make it possible to run function with multiple args

--- actions/actions.py
+++ actions/actions.py
@@ -2,36 +2,27 @@ import sys
 import actions.open_application
 import actions.open_website
 import actions.change_volume
-from configs.page_key_action_map import page_key_action_map
-
-# # func_name = list(page_key_action_map.get("pages").get("1").get("buttons").get("0").get("actions").keys())
-
-# # print(f" function = {func_name}")
-
-# # f = getattr(actions, func_name)
-
-# # f("firefox")
-
-## load all actions from the mapping dicionary
-def get_all_actions(page, button):
-    ## create a list of tupels with all the actions and args.
-    res = list(page_key_action_map.get("pages").get(f"{page}").get("buttons").get(f"{button}").get("actions").items())
-    return res
+import actions.trigger_hotkey
+import actions.change_page
 
 def perform_action(acts):
     for a, args in acts:
-        print(f"{a}, {args}")
+
         ## build module name string
         mod_str = f"actions.{a}"
         ##load the moudule from actions
         mod = sys.modules.get(mod_str)
-        print(f"{mod}")
+
 
         ## build a function with the module and function name
         f_to_run = getattr(mod, a)
 
+        print(f"args = {args}")
+        args_l = args.split(",")
+        print(f"all_args = {args_l}")
+
         ## rinally run the function
-        f_to_run(args)
+        f_to_run(*args_l)
 
 
 
