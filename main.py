5b3eca92c48beb0ac489bf3d7a22c0090a374255
ananta-lamichhane <bloomsnruins@gmail.com>
2025-03-25 21:12:27 +0100

    clean up, move initialisation to deckhandler class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from StreamDeck.DeviceManager import DeviceManager
 import threading
 from deckhandler import DeckHandler
+from key import Key
 
 
 def main():
@@ -10,29 +11,13 @@ def main():
 
     for index, deck in enumerate(streamdecks):
         deckhandler = DeckHandler(deck, "default", "1", "configs/page_key_action_map.json", 30)
-        deck.close()
-        # This example only works with devices that have screens.
-        if not deck.is_visual():
-            continue
-        try:
-            deck.open()
-            deck.reset()
-        except Exception as e:
-            print("Error opening deck. {e}")
-            deck.close()
-            deck.open()
 
         print("Opened '{}' device (serial number: '{}', fw: '{}')".format(
             deck.deck_type(), deck.get_serial_number(), deck.get_firmware_version()
         ))
 
-        # Set initial screen brightness to 30%.
-        deck.set_brightness(deckhandler.get_brightness())
-
-        # Set initial key images.
+        # # Register callback function for when a key state changes.
         deckhandler.render_full_page()
-
-        # Register callback function for when a key state changes.
         deck.set_key_callback(deckhandler.key_change_callback)
 
         # Wait until all application threads have terminated (for this example,
