4727c79c611e3466c639fd94b29e1dec39e60a88
ananta-lamichhane <bloomsnruins@gmail.com>
2025-03-25 21:15:01 +0100

    remove individual action functions

--- actions/change_page.py
+++
@@ -1,4 +0,0 @@
-## TODO: find appropriate way to trigger page change
-## needs modification to the deckhandler object
-def change_page(page_number):
-    return page_number
--- actions/open_website.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-def open_website(name):
-    subprocess.run(["firefox", name])
